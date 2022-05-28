24cdf2e391520e16ca3df48baaf77b94d1ad05ff
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-29 08:45:42 +1000

    Create userschema and timestamps

--- api/src/modules/user/User.schema.js
+++ api/src/modules/user/User.schema.js
@@ -1,14 +1,23 @@
 import mongoose from "mangoose";
 
-const UserSchema = new mangoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxLength: [50, "Name must be less than 50 characters"],
+const UserSchema = new mangoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: [50, "Name must be less than 50 characters"],
+    },
+    email: {
+      type: String,
+      required: true,
+      maxLength: [50, "Name must be less than 50 characters"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [5, "Password must be more than 5 characters"],
+      maxLength: [50, "Name must be less than 50 characters"],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    maxLength: [50, "Name must be less than 50 characters"],
-  },
-});
+  { timestamps: true }
+); // it will create created app date and updated date
