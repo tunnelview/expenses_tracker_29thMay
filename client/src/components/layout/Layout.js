9e5d0f4865ac8c928c2db1aa8dcb772d532d1636
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-29 12:38:44 +1000

    Remove dependencies

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -5,12 +5,12 @@ export const Layout = ({ children }) => {
   return (
     <div>
       {/* // Header Section */}
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="primary" expand="lg">
         <Container>
           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="ms-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
             </Nav>
@@ -22,8 +22,8 @@ export const Layout = ({ children }) => {
       <main classname="container wrapper">{children}</main>
 
       {/* footer section */}
-      <footer className="text-dark.text-center.py-5">
-        All right reserved by Shibn Abraham
+      <footer className="bg-dark text-light text-center py-5">
+        All right reserved by & Shibn Abraham
       </footer>
     </div>
   );
